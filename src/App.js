e9b05dd21fd6be02e2a10cbba429a401965858a2
nirmalh <nirmalh@qburst.com>
2019-04-15 17:18:14 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactPaginate from 'react-paginate';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor (props) {
+        super(props);
+        this.state = {
+            data: [],
+            currentPage: 1,
+            totalPages: 1
+        };
+    }
+    fetchDataFromServer = (pageNumber=1) => {
+        axios.get("https://demo.ghost.io/ghost/api/v2/content/posts/", {
+            params: {
+                key: "22444f78447824223cefc48062",
+                limit: 5,
+                page: pageNumber
+            }
+        })
+        .then(({data}) => {
+            let users, meta, totalPages, currentPage;
+            ({posts: users, meta} = data);
+            ({pages: totalPages, page: currentPage} = meta.pagination);
+            this.setState({
+                data: users,
+                totalPages: totalPages,
+                currentPage: currentPage
+            });
+        });
+    }
+    componentDidMount () {
+        this.fetchDataFromServer();
+    }
+    handlePageClick = ({selected}) => {
+        this.fetchDataFromServer(selected + 1);
+    }
+    render () {
+        return (
+            <div className="app">
+                <div className="paginate-wrapper">
+                    <ReactPaginate
+                        previousLabel={'previous'}
+                        nextLabel={'next'}
+                        breakLabel={'...'}
+                        breakClassName={'break-me'}
+                        pageClassName={'page'}
+                        disabledClassName={'disabled-page'}
+                        pageCount={this.state.totalPages}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={this.handlePageClick}
+                        containerClassName={'pagination'}
+                        activeClassName={'active'}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
